c51aff723cbfc764e05c5aec1bd51c99d423b7b3
APO19877 <APO19877@open-groupe.com>
2024-10-01 13:44:34 +0300

    add docker and last minute changes

---
+++ src/main/java/com/example/announcements_service/security/JwtConfig.java
@@ -0,0 +1,15 @@
+package com.example.announcements_service.security;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class JwtConfig {
+
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+
+
+}
